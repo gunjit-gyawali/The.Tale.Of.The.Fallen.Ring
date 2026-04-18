c090073a129f5ebe2e3c67fd99630b506750d4c1
Gunjit Gyawali <gunjitgyawali1@gmail.com>
2026-04-18 14:42:03 +0545

    first commit

---
+++ chapter.js
@@ -0,0 +1,13 @@
+const select = document.getElementById('chapterSelect');
+select.addEventListener('change', function() {
+    window.location.href = this.value;
+});
+
+function setSelectToCurrentPage() {
+    const currentPage = window.location.pathname.split('/').pop();
+    if (select.value !== currentPage) {
+        select.value = currentPage;
+    }
+}
+
+window.addEventListener('pageshow', setSelectToCurrentPage);
